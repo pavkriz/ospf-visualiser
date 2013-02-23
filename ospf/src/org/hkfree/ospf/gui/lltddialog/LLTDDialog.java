9e5fbc5cc8fda6652a8537b6785aeeeedfeba74b
schovjan <schovjan@d679fa79-cf0c-7ea4-a005-2d0338156784>
2013-02-23 23:27:33 +0000

    prijem LLTD dat

---
+++ ospf/src/org/hkfree/ospf/gui/lltddialog/LLTDDialog.java
@@ -0,0 +1,15 @@
+package org.hkfree.ospf.gui.lltddialog;
+
+import javax.swing.JDialog;
+
+import org.hkfree.ospf.model.lltd.LLTDModel;
+
+public class LLTDDialog extends JDialog {
+
+    private static final long serialVersionUID = 1L;
+    
+    
+    public LLTDDialog(LLTDModel lltd) {
+	
+    }
+}
---
+++ ospf/src/org/hkfree/ospf/tools/load/LLTDLoader.java
@@ -0,0 +1,124 @@
+package org.hkfree.ospf.tools.load;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.hkfree.ospf.model.Constants;
+import org.hkfree.ospf.model.lltd.Device;
+import org.hkfree.ospf.model.lltd.LLTDModel;
+import org.hkfree.ospf.model.lltd.Relation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * Třída pro načítání, parsování a vrácení LLTD modelů.
+ * @author Jan Schovánek
+ */
+public class LLTDLoader {
+
+    public LLTDLoader() {}
+
+
+    /**
+     * Stažení a načteni LLTD dat
+     * @param lltdModels
+     * @throws IOException
+     * @throws SAXException
+     * @throws ParserConfigurationException
+     */
+    public static List<LLTDModel> loadLLTDData() throws IOException, ParserConfigurationException,
+	    SAXException {
+	List<LLTDModel> result = new ArrayList<LLTDModel>();
+	// stazeni dat
+	URL url = new URL(Constants.URL_LLTD_DATA);
+	URLConnection conn = url.openConnection();
+	BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	String line;
+	List<String> modelsLines = new ArrayList<String>();
+	// ziskani radku obsahujici modely
+	while ((line = br.readLine()) != null) {
+	    if (line.endsWith("</lltd></td></tr>")) // takto konci radek obsahujici retezec LLTD modelu
+		modelsLines.add(line);
+	}
+	// rozparsovani radku a ziskani modelu
+	for (String modelString : modelsLines) {
+	    result.add(loadModel(modelString));
+	}
+	return result;
+    }
+
+
+    /**
+     * Rozparsuje řádek a vrací obsahující LLTD model
+     * @param line řádek obsahující model
+     * @return LLTDModel
+     * @throws ParserConfigurationException
+     * @throws IOException
+     * @throws SAXException
+     */
+    private static LLTDModel loadModel(String line) throws ParserConfigurationException, SAXException, IOException {
+	LLTDModel model = new LLTDModel();
+	Pattern pattern = Pattern.compile("<\\?xml.*</lltd>");
+	Matcher matcher = pattern.matcher(line);
+	matcher.find();
+	String modelS = matcher.group(0);
+	DocumentBuilder newDocumentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+	Document doc = newDocumentBuilder.parse(new ByteArrayInputStream(modelS.getBytes()));
+	// normalize text representation, doporucuje se to...
+	doc.getDocumentElement().normalize();
+	NodeList elems = doc.getElementsByTagName("device");
+	NodeList links = doc.getElementsByTagName("relation");
+	model.setDate(new Date(Long.valueOf(doc.getDocumentElement().getAttribute("millis"))));
+	model.setPublicIP(doc.getDocumentElement().getAttribute("publicIP"));
+	List<Device> devices = new ArrayList<Device>();
+	List<Relation> relations = new ArrayList<Relation>();
+	Device d = null;
+	Relation r = null;
+	for (int i = 0; i < elems.getLength(); i++) {
+	    Element e = (Element) elems.item(i);
+	    d = new Device();
+	    d.setSource(e.getAttribute("mask"));
+	    d.setMachineName(e.getElementsByTagName("machineName").item(0).getTextContent());
+	    d.setIpv4(e.getElementsByTagName("ipv4").item(0).getTextContent());
+	    d.setIpv6(e.getElementsByTagName("ipv6").item(0).getTextContent());
+	    devices.add(d);
+	}
+	for (int i = 0; i < links.getLength(); i++) {
+	    Element e = (Element) links.item(i);
+	    r = new Relation();
+	    r.setFrom(getDevice(e.getAttribute("from"), devices));
+	    r.setTo(getDevice(e.getAttribute("to"), devices));
+	    r.setMedium(e.getElementsByTagName("medium").item(0).getTextContent());
+	    relations.add(r);
+	}
+	model.setDevices(devices);
+	model.setRelations(relations);
+	return model;
+    }
+
+
+    private static Device getDevice(String name, List<Device> devices) {
+	for (Device d : devices) {
+	    if (d.getSource().equals(name)) {
+		return d;
+	    }
+	}
+	System.out.println(name);
+	return null;
+    }
+}
